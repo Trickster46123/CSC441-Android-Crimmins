d78fe95cb5d2802ac809d0c4f229a8e2e2cdb2a3
Sam Crimmins <samcrimm@yahoo.com>
2026-09-25 01:51:40 -0400

    S5 done

--- app/src/main/java/edu/lemoyne/campusapp/MainActivity.kt
+++ app/src/main/java/edu/lemoyne/campusapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CampusAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Sam",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
